03166876fe9c32cf9d638845c1ede013321a1f19
astoisavljevic <astoisavljevic@jatheon.com>
2024-04-16 21:48:29 +0200

    initial commit

---
+++ 001-integer-equals/src/main/java/rs/in/staleksit/hello/Example1.java
@@ -0,0 +1,16 @@
+package rs.in.staleksit.hello;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+        Integer i = 260;
+        Integer j = 260;
+
+        if (i == j) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+
+}
